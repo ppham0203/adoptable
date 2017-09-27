8a1a6b5c9a04d58c47826efce9aa7b4d085afd9b
Andrew Nguyen <andrewnguyen14@gmail.com>
2017-09-27 15:21:35 -0700

    updates to file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,94 +1,19 @@
 import React, { Component } from "react";
-import Jumbotron from "./components/Index/Jumbotron";
-import Nav from "./components/Index/Nav";
-import Input from "./components/Index/Input";
-import Button from "./components/Index/Button";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Jumbotron from "./components/Jumbotron";
+import Nav from "./components/Nav";
+import Input from "./components/Input";
+import Button from "./components/Button";
 import API from "./utils/API";
-import { RecipeList, RecipeListItem } from "./components/Index/RecipeList";
-import { Container, Row, Col } from "./components/Index/Grid";
-
-class App extends Component {
-  state = {
-    recipes: [],
-    recipeSearch: ""
-  };
-
-  handleInputChange = event => {
-    // Destructure the name and value properties off of event.target
-    // Update the appropriate state
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
-
-  handleFormSubmit = event => {
-    // When the form is submitted, prevent its default behavior, get recipes update the recipes state
-    event.preventDefault();
-    API.getRecipes(this.state.recipeSearch)
-      .then(res => this.setState({ recipes: res.data }))
-      .catch(err => console.log(err));
-  };
-
-  render() {
-    return (
-      <div>
-        <Nav />
-        <Jumbotron />
-        <Container>
-          <Row>
-            <Col size="md-12">
-              <form>
-                <Container>
-                  <Row>
-                    <Col size="xs-9 sm-10">
-                      <Input
-                        name="petSearch"
-                        value={this.state.petSearch}
-                        onChange={this.handleInputChange}
-                        placeholder="Zip Code"
-                      />
-                    </Col>
-                    <Col size="xs-3 sm-2">
-                      <Button
-                        onClick={this.handleFormSubmit}
-                        type="success"
-                        className="input-lg"
-                      >
-                        Search
-                      </Button>
-                    </Col>
-                  </Row>
-                </Container>
-              </form>
-            </Col>
-          </Row>
-          <Row>
-            <Col size="xs-12">
-              {!this.state.recipes.length ? (
-                <h1 className="text-center">No Recipes to Display</h1>
-              ) : (
-                <RecipeList>
-                  {this.state.recipes.map(recipe => {
-                    return (
-                      <RecipeListItem
-                      hotdog="hotdog"
-                        key={recipe.title}
-                        title={recipe.title}
-                        href={recipe.href}
-                        ingredients={recipe.ingredients}
-                        thumbnail={recipe.thumbnail}
-                      />
-                    );
-                  })}
-                </RecipeList>
-              )}
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
-}
-
+import homepage from 
+import { RecipeList, RecipeListItem } from "./components/RecipeList";
+import { Container, Row, Col } from "./components/Grid";
+const App = () =>
+  <Router>
+    <div>
+      <Route exact path="/" component={homepage} />
+      <Route exact path="/adoptionpage" component={adoptionPage} />
+    </div>
+  </Router>;
+  
 export default App;

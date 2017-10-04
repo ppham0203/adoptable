235a0b64cb60de7b76dd0336465e57daed78052c
Peter Pham <ppham0203@yahoo.com>
2017-10-04 13:03:48 -0700

    reformat homepage

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -48,65 +48,8 @@ class App extends Component {
       <div>
         <Nav />
         <Jumbotron />
-<<<<<<< HEAD
-        <Container>
-          <Row>
-            <Col size="md-12">
-              <form>
-                <Container>
-                  <Row>
-                    <Col size="xs-0 sm-2">
-                   
-                    </Col>
-                    <Col size="xs-9 sm-4">
-                      <Input
-                        name="petSearch"
-                        value={this.state.petSearch}
-                        onChange={this.handleInputChange}
-                        placeholder="Zip Code"
-                      />
-
-                      {/* <MenuItem onClick={()=> this.setState({breed: “Chihuahua”}).bind(this)} */}
-                      
-                    </Col>
-                    <Col size="xs-9 sm-4">
-                     <DropdownButton bsSize="large" title="Breed" id="dropdown-size-large">
-                      <MenuItem onClick={()=> this.setState({breed: "Chihuahua SH MIX"}).bind(this)}>Chihuahua</MenuItem>
-                      <MenuItem eventKey="2">Pitbull</MenuItem>
-                      <MenuItem eventKey="3">Shih Tsu</MenuItem>
-                      <MenuItem eventKey="4">Terrier</MenuItem>
-                    </DropdownButton>
-                    <DropdownButton bsSize="large" title="Gender" id="dropdown-size-large">
-                      <MenuItem eventKey="1">Male</MenuItem>
-                      <MenuItem eventKey="2">Female</MenuItem>
-                    </DropdownButton>
-                      <Button
-                        bsSize="large" 
-                        title="Search"
-                        onClick={this.handleFormSubmit}
-                        type="success"
-                        className="input-lg"
-                      >
-                      <Link to="/petresults">Search</Link>
-                      </Button>
-                  
-                    </Col>
-                  </Row>
-                  <Col size=" sm-12">
-
-                  <Search
-                      
-                       />
-                       </Col>
-                </Container>
-              </form>
-            </Col>
-          </Row>
-         
-=======
         <Container>                   
           <Search />    
->>>>>>> master
         </Container>
       </div>
 

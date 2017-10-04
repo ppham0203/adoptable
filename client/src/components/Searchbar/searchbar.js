fa23fbd8fe95d8db5a89169dab051586279e96fa
Peter Pham <ppham0203@yahoo.com>
2017-10-04 13:11:50 -0700

    edit to searchbar.js

--- client/src/components/Searchbar/searchbar.js
+++ client/src/components/Searchbar/searchbar.js
@@ -3,35 +3,6 @@ import DogList from "../Doglist";
 import { Row, Col, Container } from "../Grid";
 import "./searchbar.css";
 // RecipeList renders a bootstrap list item
-<<<<<<< HEAD
- const SearchBar = (props) => (
-<div>
-<select
-      name="breed"
-      value={props.breed}
-      onChange={props.handleChange}
-    >
-      <option value="PIT BULL">Pitbull </option>
-      <option value="terrier">Terrier </option>
-      <option value="Chihuahua SH MIX">Chihuahua</option>
-      <option value="GERM SHEPHERD MIX">German Shepherd</option>
-      <option value="POMERANIAN">Pomeranian</option>
-      <option value="YORKSHIRE TERR">Yorkshire Terrier</option>
-   
-
-    </select>
-
-
-    <select
-      name="gender"
-      value={props.gender}
-      onChange={props.handleChange}
-    >
-      <option value="M">Male</option>
-      <option value="S">Female</option>
-      
-    </select>
-=======
 const SearchBar = (props) => (
   <div>
     <select className="btn-lg btn-group-lg btn"
@@ -55,7 +26,6 @@ const SearchBar = (props) => (
         <option value="S">Female </option>
       </select>
     </div>
->>>>>>> master
 
 
 

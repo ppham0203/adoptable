386dd2966797da4eddc1bdee1b1fb31717c31cab
Andrew Nguyen <andrewnguyen14@gmail.com>
2017-10-05 10:46:08 -0700

    cleaned up pet profile

--- client/src/components/Petprofile/petProfile.js
+++ client/src/components/Petprofile/petProfile.js
@@ -10,19 +10,8 @@ class Petprofile extends Component {
   state = {
     dogs: {}
   };
-  // When this component mounts, grab the book with the _id of this.props.match.params.id
-  // e.g. localhost:3000/books/599dcb67f0f16317844583fc
+ 
   componentDidMount() {
-    // axios.get('/adoptionPets/:id').then( results => {
-        
-    //     const dogs = results.data;
-    //     console.log("Axios results", results);
-    //     const newState = {
-    //       dogs: dogs // array of dogs returned from server
-    //     }
-    //     this.setState(newState)
-    //   });
-
       API.getPet(this.props.match.params.id)
       .then(res => this.setState({ dogs: res.data }))
       .catch(err => console.log(err));
@@ -34,11 +23,6 @@ class Petprofile extends Component {
            <Nav />
         <Row>
           <Col size="md-12">
-            {/* <Jumbotron>
-              <h1>
-                {this.state.dogs.name}
-              </h1>
-            </Jumbotron> */}
           </Col>
         </Row>
         <Row>
@@ -51,17 +35,11 @@ class Petprofile extends Component {
             </article>
           </Col>
           <Col size="md 6">
-          <h1> About {this.state.dogs.name} </h1>
-          <ul>
-              <li>Name: {this.state.dogs.name} </li>
+          <h1> About Me </h1>
               <li>Breed: {this.state.dogs.breed} </li>
               <li>Gender: {this.state.dogs.gender}</li>
               <li>Age: {this.state.dogs.age}</li>
-              <li> <a href={this.state.dogs.link}>Give this dog a home!</a></li>
-          </ul>
-             
-              
-              
+              <li> <a href={this.state.dogs.link}>Give me a home!</a></li>
           </Col>
         </Row>
       </Container>
